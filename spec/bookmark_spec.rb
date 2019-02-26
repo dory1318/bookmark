b5bc64c164e9f2d3194f68dba08c5fbdce209d27
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-02-26 15:47:45 +0000

    pass unit/feature tests for adding a new bookmark

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -14,5 +14,11 @@ describe Bookmark do
       expect(bookmarks).to include('http://www.destroyallsoftware.com')
       expect(bookmarks).to include('http://www.google.com')
  end
+
+it 'creates a new bookmark' do
+  Bookmark.create(url: 'https://example.com')
+  expect(Bookmark.all).to include 'https://example.com'
+ end
+
 end
 end
